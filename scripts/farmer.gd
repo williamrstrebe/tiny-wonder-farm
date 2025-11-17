72816c548bd4c69764d7d163b087a651ec36ed39
William R. Strebe <william.strebe@kbase.com.br>
2025-11-16 22:45:12 -0300

    frame adjustments for idle anim and walk speed

--- scripts/farmer.gd
+++ scripts/farmer.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
+const SPEED = 125.0
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
+	#Exemplo TIMEOUT / await
+	#await get_tree().create_timer(3.0).timeout
+	
 	# Add the gravity.
 	#if not is_on_floor():
 	#	velocity += get_gravity() * delta
