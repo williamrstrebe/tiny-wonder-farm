c4d008a53fc8e757d8cf0e61f1172bc04ec6b329
William R. Strebe <william.strebe@kbase.com.br>
2025-11-28 14:53:11 -0300

    Multiple scene adjustments

--- scripts/farmer.gd
+++ scripts/farmer.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 125.0
 const JUMP_VELOCITY = -400.0
 
@@ -10,15 +9,11 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	#Exemplo TIMEOUT / await
 	#await get_tree().create_timer(3.0).timeout
-	
-	
-		
-
 	var direction := Input.get_vector("left", "right", "up", "down")
 	if !Input.is_action_pressed("left") && !Input.is_action_pressed("right") && !Input.is_action_pressed("up") && !Input.is_action_pressed("down"):
 		direction = Vector2.ZERO
 		$AnimatedSprite2D.play("idle")
-			
+
 	direction = direction.normalized()
 	velocity = direction * SPEED
 	move_and_slide()
@@ -31,5 +26,10 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.play("walk_up")
 	elif Input.is_action_pressed("down"):
 		$AnimatedSprite2D.play("walk_down")
-		
-		
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print(body.name)
+	if (body.name == "farmer_main"):
+		get_tree().change_scene_to_file.bind("res://scenes/house_interior.tscn").call_deferred()
+	elif (body.name == "farmer_interior"):
+		get_tree().change_scene_to_file.bind("res://scenes/main_scene.tscn").call_deferred()
--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 var strawberry = preload("res://scenes/strawberry.tscn")
 
 var time
@@ -10,7 +9,7 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	time = Time.get_datetime_dict_from_system()
-	$farmer/Label.text = str("%02d"%time.hour, ":", "%02d"%time.minute)
+	$farmer_main/Label.text = str("%02d"%time.hour, ":", "%02d"%time.minute)
 	
 	if Input.is_key_pressed(KEY_E):
 		var instance = strawberry.instantiate()
